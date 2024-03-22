abf09608aa1c2b04c1292574f64759f803e9beca
kumbharkajal2004 <kumbharkajal497@gmail.com>
2024-03-22 11:17:05 +0530

    pro1

---
+++ isStringorNot.js
@@ -0,0 +1,11 @@
+let str = "kajal";
+let result = isInput(str);
+function isInput(str) {
+  let abc = typeof str;
+  if (abc === "string") return abc;
+}
+console.log(result);
+
+let boolean = true;
+let str1 = 80;
+let string = "shreeeeeee";
